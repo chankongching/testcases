8bc9e4092021830d1e9d9cf62e26722627031854
Kong Ching <jackychan@eubchain.com>
2018-04-24 10:38:17 +0800

    Adding images to the right location

--- distributed.py
+++ distributed.py
@@ -8,16 +8,16 @@ from tensorflow.examples.tutorials.mnist import input_data
 flags = tf.app.flags
 IMAGE_PIXELS = 28
 # 定义默认训练参数和数据路径
-flags.DEFINE_string('data_dir', '/tmp/mnist-data', 'Directory  for storing mnist data')
+flags.DEFINE_string('data_dir', 'MNIST_data/', 'Directory  for storing mnist data')
 flags.DEFINE_integer('hidden_units', 100, 'Number of units in the hidden layer of the NN')
 flags.DEFINE_integer('train_steps', 10000, 'Number of training steps to perform')
 flags.DEFINE_integer('batch_size', 100, 'Training batch size ')
 flags.DEFINE_float('learning_rate', 0.01, 'Learning rate')
 # 定义分布式参数
 # 参数服务器parameter server节点
-flags.DEFINE_string('ps_hosts', '192.168.32.145:22221', 'Comma-separated list of hostname:port pairs')
+flags.DEFINE_string('ps_hosts', '192.168.1.173:2222', 'Comma-separated list of hostname:port pairs')
 # 两个worker节点
-flags.DEFINE_string('worker_hosts', '192.168.32.146:22221,192.168.32.160:22221',
+flags.DEFINE_string('worker_hosts', '192.168.1.171:2222,192.168.1.172:2222',
                     'Comma-separated list of hostname:port pairs')
 # 设置job name参数
 flags.DEFINE_string('job_name', None, 'job name: worker or ps')
@@ -50,73 +50,78 @@ def main(unused_argv):
     server = tf.train.Server(cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
     if FLAGS.job_name == 'ps':
         server.join()
+    elif FLAGS.job_name == "worker":
 
-    is_chief = (FLAGS.task_index == 0)
-    # worker_device = '/job:worker/task%d/cpu:0' % FLAGS.task_index
-    with tf.device(tf.train.replica_device_setter(
-            cluster=cluster
-    )):
-        global_step = tf.Variable(0, name='global_step', trainable=False)  # 创建纪录全局训练步数变量
+      is_chief = (FLAGS.task_index == 0)
+      # worker_device = '/job:worker/task%d/cpu:0' % FLAGS.task_index
+          # Between-graph replication
+      with tf.device(tf.train.replica_device_setter(
+      worker_device="/job:worker/task:%d" % FLAGS.task_index,
+      cluster=cluster)):
+  #     with tf.device(tf.train.replica_device_setter(
+  #            cluster=cluster
+  #    )):
+          global_step = tf.Variable(0, name='global_step', trainable=False)  # 创建纪录全局训练步数变量
+  
+          hid_w = tf.Variable(tf.truncated_normal([IMAGE_PIXELS * IMAGE_PIXELS, FLAGS.hidden_units],
+                                                  stddev=1.0 / IMAGE_PIXELS), name='hid_w')
+          hid_b = tf.Variable(tf.zeros([FLAGS.hidden_units]), name='hid_b')
 
-        hid_w = tf.Variable(tf.truncated_normal([IMAGE_PIXELS * IMAGE_PIXELS, FLAGS.hidden_units],
-                                                stddev=1.0 / IMAGE_PIXELS), name='hid_w')
-        hid_b = tf.Variable(tf.zeros([FLAGS.hidden_units]), name='hid_b')
-
-        sm_w = tf.Variable(tf.truncated_normal([FLAGS.hidden_units, 10],
-                                               stddev=1.0 / math.sqrt(FLAGS.hidden_units)), name='sm_w')
-        sm_b = tf.Variable(tf.zeros([10]), name='sm_b')
-
-        x = tf.placeholder(tf.float32, [None, IMAGE_PIXELS * IMAGE_PIXELS])
-        y_ = tf.placeholder(tf.float32, [None, 10])
-
-        hid_lin = tf.nn.xw_plus_b(x, hid_w, hid_b)
-        hid = tf.nn.relu(hid_lin)
-
-        y = tf.nn.softmax(tf.nn.xw_plus_b(hid, sm_w, sm_b))
-        cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
-
-        opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
-
-        train_step = opt.minimize(cross_entropy, global_step=global_step)
-        # 生成本地的参数初始化操作init_op
-        init_op = tf.global_variables_initializer()
-        train_dir = tempfile.mkdtemp()
-        sv = tf.train.Supervisor(is_chief=is_chief, logdir=train_dir, init_op=init_op, recovery_wait_secs=1,
+          sm_w = tf.Variable(tf.truncated_normal([FLAGS.hidden_units, 10],
+                                                 stddev=1.0 / math.sqrt(FLAGS.hidden_units)), name='sm_w')
+          sm_b = tf.Variable(tf.zeros([10]), name='sm_b')
+  
+          x = tf.placeholder(tf.float32, [None, IMAGE_PIXELS * IMAGE_PIXELS])
+          y_ = tf.placeholder(tf.float32, [None, 10])
+  
+          hid_lin = tf.nn.xw_plus_b(x, hid_w, hid_b)
+          hid = tf.nn.relu(hid_lin)
+  
+          y = tf.nn.softmax(tf.nn.xw_plus_b(hid, sm_w, sm_b))
+          cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
+  
+          opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
+  
+          train_step = opt.minimize(cross_entropy, global_step=global_step)
+          # 生成本地的参数初始化操作init_op
+          init_op = tf.global_variables_initializer()
+          train_dir = tempfile.mkdtemp()
+          sv = tf.train.Supervisor(is_chief=is_chief, logdir=train_dir, init_op=init_op, recovery_wait_secs=1,
                                  global_step=global_step)
 
-        if is_chief:
-            print 'Worker %d: Initailizing session...' % FLAGS.task_index
-        else:
-            print 'Worker %d: Waiting for session to be initaialized...' % FLAGS.task_index
-        sess = sv.prepare_or_wait_for_session(server.target)
-        print 'Worker %d: Session initialization  complete.' % FLAGS.task_index
+          if is_chief:
+              print 'Worker %d: Initailizing session...' % FLAGS.task_index
+          else:
+              print 'Worker %d: Waiting for session to be initaialized...' % FLAGS.task_index
+          sess = sv.prepare_or_wait_for_session(server.target)
+          print 'Worker %d: Session initialization  complete.' % FLAGS.task_index
+  
+          time_begin = time.time()
+          print 'Traing begins @ %f' % time_begin
 
-        time_begin = time.time()
-        print 'Traing begins @ %f' % time_begin
+          local_step = 0
+          while True:
+              batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batch_size)
+              train_feed = {x: batch_xs, y_: batch_ys}
+  
+              _, step = sess.run([train_step, global_step], feed_dict=train_feed)
+              local_step += 1
+  
+              now = time.time()
+              print '%f: Worker %d: traing step %d dome (global step:%d)' % (now, FLAGS.task_index, local_step, step)
+  
+              if step >= FLAGS.train_steps:
+                  break
 
-        local_step = 0
-        while True:
-            batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batch_size)
-            train_feed = {x: batch_xs, y_: batch_ys}
+          time_end = time.time()
+          print 'Training ends @ %f' % time_end
+          train_time = time_end - time_begin
+          print 'Training elapsed time:%f s' % train_time
 
-            _, step = sess.run([train_step, global_step], feed_dict=train_feed)
-            local_step += 1
-
-            now = time.time()
-            print '%f: Worker %d: traing step %d dome (global step:%d)' % (now, FLAGS.task_index, local_step, step)
-
-            if step >= FLAGS.train_steps:
-                break
-
-        time_end = time.time()
-        print 'Training ends @ %f' % time_end
-        train_time = time_end - time_begin
-        print 'Training elapsed time:%f s' % train_time
-
-        val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
-        val_xent = sess.run(cross_entropy, feed_dict=val_feed)
-        print 'After %d training step(s), validation cross entropy = %g' % (FLAGS.train_steps, val_xent)
-    sess.close()
+          val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
+          val_xent = sess.run(cross_entropy, feed_dict=val_feed)
+          print 'After %d training step(s), validation cross entropy = %g' % (FLAGS.train_steps, val_xent)
+      sess.close()
 
 if __name__ == '__main__':
     tf.app.run()
